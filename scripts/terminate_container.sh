6e9764c2cf1f6f40c11b1cf5a6fe6c1a7fca4467
seung-woo-ryu <tmddn645@naver.com>
2024-10-17 09:21:13 +0900

    [main] scripts 수정

--- scripts/terminate_container.sh
+++ scripts/terminate_container.sh
@@ -3,3 +3,6 @@
 docker stop was-container
 docker rm -f was-container
 docker rmi 439127579128.dkr.ecr.ap-northeast-2.amazonaws.com/klol/was:latest
+
+Stop one or more running containers
+Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
